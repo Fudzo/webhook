b52502a1fc33d57ad9c7327ffc383819330c087f
Nikola Furundzic <nfurundzic@Nikolas-MacBook-Pro.local>
2024-09-09 11:33:46 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 5555;
+
+app.use(express.json());
+
+
+app.post('/viber-webhook', (req, res) => {
+    const event = req.body;
+    console.log('Received event:', event);
+    res.status(200).send();
+});
+
+const setViberWebhook = async () => {
+    try {
+        const response = await axios.post('https://chatapi.viber.com/pa/set_webhook', {
+            url: process.env.WEBHOOK_URL,  // Replace with your public URL
+            auth_token: VIBER_BOT_TOKEN
+        });
+        console.log('Webhook set:', response.data);
+    } catch (error) {
+        console.error('Error setting webhook:', error);
+    }
+};
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    setViberWebhook();
+});
